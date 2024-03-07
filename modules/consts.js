8c88cd3c80c04e8800e42f058e736daa5d1efe4b
ESTARBUCKET <evzd2003@mail.ru>
2024-03-07 12:16:46 +0300

    Добавлены ajax, constants, urls

---
+++ modules/consts.js
@@ -0,0 +1,3 @@
+export const groupId = '224721117'
+export const accessToken = 'vk1.a.i6vOcX96GDwGn22ekSHG1VMruXgAtKzrvscElql2w9hbqVJ6PKbTDhlyQywRZVlRJCtYUKGxE-uxcC-7ZvUioM0VPBzCyU43mADTXU9Lwp-hXoUgMmnAK9sxGVb7VvQmC7W8N3_PNtlnvrS-8TsWehanSmbChY0_ryHYE7j8BVL9wYk1kCL-A4S7xukGaiAVjXL-PIdneRty1oIO6Yy2hg'
+export const version = '5.131'
